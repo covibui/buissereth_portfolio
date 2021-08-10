e1756164c845d1ee7a5dcf730360ebec8398d20a
Erik-Martus <ekm66@drexel.edu>
2021-08-10 12:07:05 -0400

    :wrench: Add theme font family configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,12 @@ module.exports = {
   mode: "jit",
   purge: ["./src/**/*.html", "./src/**/*.njk", "./src/**/*.svg"],
   darkMode: "media",
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["Poppins", "Robooto", "Helvetica", "Arial", "sans-serif"],
+        body: ["Roboto", "Helvetica", "Arial", "sans-serif"],
+      },
+    },
+  },
 };
