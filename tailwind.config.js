1129b71e95ddd3912857300219a35279dff37381
Erik-Martus <ekm66@drexel.edu>
2021-08-11 12:15:40 -0400

    :wrench: Add additional tailwind color options
    Add blueGray and orange color variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   mode: "jit",
   purge: ["./src/**/*.html", "./src/**/*.njk", "./src/**/*.svg"],
   darkMode: "media",
   theme: {
     extend: {
+      colors: {
+        blueGray: colors.blueGray,
+        orange: colors.orange,
+      },
       fontFamily: {
         display: ["Poppins", "Robooto", "Helvetica", "Arial", "sans-serif"],
         body: ["Roboto", "Helvetica", "Arial", "sans-serif"],
