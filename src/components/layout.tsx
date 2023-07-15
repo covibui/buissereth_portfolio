fd5d4e7a090e44d98e89a6f9bca6bd59939a5494
Erik Martus <erik@emartus.dev>
2023-07-15 15:00:38 -0400

    fix prettier

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,58 +8,59 @@ const name = "Brianna Buissereth";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({
-  children,
-  home,
+    children,
+    home,
 }: {
-  children: React.ReactNode;
-  home: any;
+    children: React.ReactNode;
+    home: any;
 }) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        {/* TODO: Replace with actual og:image */}
-        {/* <meta
+    return (
+        <div className={styles.container}>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="description"
+                    content="Learn how to build a personal website using Next.js"
+                />
+                {/* TODO: Replace with actual og:image */}
+                {/* <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         /> */}
-        <meta name="og:title" content={siteTitle} />
-        {/* TODO: Replace with actual twitter:card */}
-        {/* <meta name="twitter:card" content="summary_large_image" /> */}
-      </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2XL}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <Image
-                priority
-                src="/images/profile.jpg"
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}alt={name}
-              />
-            </Link>
-          </>
-        )}
-      </header>
-    </div>
-  );
+                <meta name="og:title" content={siteTitle} />
+                {/* TODO: Replace with actual twitter:card */}
+                {/* <meta name="twitter:card" content="summary_large_image" /> */}
+            </Head>
+            <header className={styles.header}>
+                {home ? (
+                    <>
+                        <Image
+                            priority
+                            src="/images/profile.jpg"
+                            className={utilStyles.borderCircle}
+                            height={144}
+                            width={144}
+                            alt={name}
+                        />
+                        <h1 className={utilStyles.heading2XL}>{name}</h1>
+                    </>
+                ) : (
+                    <>
+                        <Link href="/">
+                            <Image
+                                priority
+                                src="/images/profile.jpg"
+                                className={utilStyles.borderCircle}
+                                height={108}
+                                width={108}
+                                alt={name}
+                            />
+                        </Link>
+                    </>
+                )}
+            </header>
+        </div>
+    );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,26 @@
 import Head from "next/head";
 import utilStyles from "@/styles/utils.module.css";
+import Layout, { siteTitle } from "@/components/layout";
 
 export default function Home() {
-  return (
-    <>
-      <Layout home>
-        <Head>
-          <title>{siteTitle}</title>
-        </Head>
-        <section className={utilStyles.headingMd}>
-          <p>[Your Self Introduction]</p>
-          <p>
-            (This is a sample website - you’ll be building a site like this in{" "}
-            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-          </p>
-        </section>
-      </Layout>
-    </>
-  );
+    return (
+        <>
+            <Layout home>
+                <Head>
+                    <title>{siteTitle}</title>
+                </Head>
+                <section className={utilStyles.headingMd}>
+                    <p>[Your Self Introduction]</p>
+                    <p>
+                        (This is a sample website - you’ll be building a site
+                        like this in{" "}
+                        <a href="https://nextjs.org/learn">
+                            our Next.js tutorial
+                        </a>
+                        .)
+                    </p>
+                </section>
+            </Layout>
+        </>
+    );
 }
