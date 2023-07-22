a235a8514a0d01f9ba98998ea0201d2fbe16524b
Erik Martus <erik@emartus.dev>
2023-07-22 17:48:19 -0400

    attempt fix build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Layout from "@/components/Layout";
+import Layout from "../components/Layout";
 import { GetStaticProps } from "next";
 import { ProjectFrontMatter, ProjectType } from "@/types";
 import Link from "next/link";
