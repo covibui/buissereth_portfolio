b7a9b13c76670f938b06cda07a36557d5fe1ca53
Erik Martus <erik@emartus.dev>
2023-09-16 21:12:10 -0400

    refactor to use CSS grid instead of MUI Grid v2

--- src/components/ProjectContentSection/ProjectTwoColumnSection.tsx
+++ src/components/ProjectContentSection/ProjectTwoColumnSection.tsx
@@ -1,7 +1,6 @@
 import { TwoColumnSection } from "@/types";
 import convertToKebabCase from "@/utils/convertToKebabCase";
 import { Box, Typography, useMediaQuery } from "@mui/material";
-import Grid from "@mui/material/Unstable_Grid2";
 import DescriptionContent from "./DescriptionContent";
 import useGetImagePath from "@/hooks/useGetImagePath";
 import Image from "next/image";
@@ -19,85 +18,87 @@ export default function ProjectTwoColumnSection({ section }: Props) {
 
   const imagePath = useGetImagePath(image);
 
-  const ImageColumn = () => (
-    <Grid xs={10} md={5}>
-      <Box sx={{ display: "flex", alignItems: "center", height: 1 }}>
-        <Box
-          sx={{
-            position: "relative",
-            aspectRatio: 1 / 1.5,
-            maxWidth: {
-              xs: "75vw",
-              lg: "auto",
-            },
-            mx: "auto",
-            flexGrow: 1,
+  const ImageColumn = ({ right }: { right?: boolean }) => (
+    <Box
+      sx={{
+        gridColumn: {
+          xs: "span 10",
+          md: `${right ? "6 / " : ""}span 5`,
+        },
+        display: { md: "flex" },
+        alignItems: "center",
+      }}
+    >
+      <Box
+        sx={{
+          position: "relative",
+          aspectRatio: 1 / 1.5,
+          maxWidth: {
+            xs: "75vw",
+            lg: "auto",
+          },
+          mx: "auto",
+          flexGrow: 1,
+        }}
+      >
+        <Image
+          fill
+          src={imagePath}
+          sizes={`(max-width: ${breakpoints.md}): 50vw, 60vw`}
+          alt={image.alt}
+          style={{
+            objectFit: "contain",
+            position: "absolute",
           }}
-        >
-          <Image
-            fill
-            src={imagePath}
-            sizes={`(max-width: ${breakpoints.md}): 50vw, 60vw`}
-            alt={image.alt}
-            style={{
-              objectFit: "contain",
-              position: "absolute",
-            }}
-          />
-        </Box>
+        />
       </Box>
-    </Grid>
+    </Box>
   );
 
   const TextColumn = () => (
-    <Grid xs={10} md={5}>
-      <Box
-        sx={{
-          display: { md: "flex" },
-          flexDirection: "column",
-          justifyContent: "center",
-          alignContent: "center",
-          height: {
-            md: 1,
-          },
-        }}
-      >
-        <Typography variant="projectSubtitle">{subtitle}</Typography>
-        <DescriptionContent description={description} />
-      </Box>
-    </Grid>
+    <Box
+      sx={{
+        gridColumn: {
+          xs: "span 10",
+          md: "span 4",
+        },
+        display: { md: "flex" },
+        flexDirection: "column",
+        justifyContent: "center",
+        alignContent: "center",
+        height: {
+          md: 1,
+        },
+      }}
+    >
+      <Typography variant="projectSubtitle">{subtitle}</Typography>
+      <DescriptionContent description={description} />
+    </Box>
   );
 
   return (
     <AppContainer>
       <Box
+        component="section"
+        id={convertToKebabCase(subtitle)}
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
+          display: "grid",
+          gridTemplateColumns: "repeat(10, 1fr)",
+          columnGap: 2.5,
           minHeight: { md: "100vh" },
         }}
       >
-        <Grid
-          component="section"
-          id={convertToKebabCase(subtitle)}
-          container
-          spacing={5}
-          columns={10}
-          sx={{ flexGrow: 1 }}
-        >
-          {variant === "left" && isScreenMd ? (
-            <>
-              <ImageColumn />
-              <TextColumn />
-            </>
-          ) : (
-            <>
-              <TextColumn />
-              <ImageColumn />
-            </>
-          )}
-        </Grid>
+        {variant === "left" && isScreenMd ? (
+          <>
+            <ImageColumn />
+            <TextColumn />
+          </>
+        ) : (
+          <>
+            <TextColumn />
+            <ImageColumn right />
+          </>
+        )}
       </Box>
     </AppContainer>
   );
